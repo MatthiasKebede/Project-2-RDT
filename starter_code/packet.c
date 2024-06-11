b19fe30a03127cb85c9346d2f32474adfb887e4a
Nathan Behailu <120515756+nathanbehailuz@users.noreply.github.com>
2024-06-11 14:15:26 +0400

    Update packet.c
    added inline comments

--- starter_code/packet.c
+++ starter_code/packet.c
@@ -2,9 +2,8 @@
 #include"packet.h"
 
 static tcp_packet zero_packet = {.hdr={0}};
-/*
- * create TCP packet with header and space for data of size len
- */
+
+// create TCP packet with header and space for data of size len
 tcp_packet* make_packet(int len)
 {
     tcp_packet *pkt;
@@ -15,6 +14,7 @@ tcp_packet* make_packet(int len)
     return pkt;
 }
 
+// returns data size of packet
 int get_data_size(tcp_packet *pkt)
 {
     return pkt->hdr.data_size;
